bc1b50fb35c0a36d2b6584ac7c47296ab9ec0fb1
Meggymegs <suniga.miguel@yahoo.com>
2015-03-29 11:58:13 -0700

    Updates for mainpage -> fullcalendar
    having issues with connecting and fetching info from DB

---
+++ index/events.php
@@ -0,0 +1,132 @@
+<?php
+
+try {
+	
+    // Connect to database
+    include '../mysqli_connect.php';
+	session_start();
+	$myusername = $_SESSION['myusername'];
+    // Prepare and execute query
+    $query = "SELECT food_served_table.user_id, food_table.food_name,food_served_table.date_created 
+						FROM food_served_table, user_table, food_table 
+						WHERE user_table.user_email like '$myusername' 
+						AND food_served_table.user_id = user_table-user_id
+						AND food_served_table.food_id = food_table.food_id";
+    $sth = $connection->prepare($query);
+    $sth->execute();
+
+    // Returning array
+    $events = array();
+
+    // Fetch results
+    while ($row = $sth->fetch(PDO::FETCH_ASSOC) {
+
+        $e = array();
+        $e['id'] = $row['user_table.user_id'];
+        $e['title'] = $row['food_table.food_name'];
+        $e['start'] = $row['food_served_table.date_created'];
+        $e['allDay'] = false;
+
+        // Merge the event array into the return array
+        array_push($events, $e);
+
+    }
+
+    // Output json for our calendar
+    echo json_encode($events);
+    exit();
+
+} catch (PDOException $e){
+    echo $e->getMessage();
+
+/*
+// Create connection
+include '../mysqli_connect.php';
+
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+session_start();
+
+$result = mysql_query("SELECT food_served_table.user_id, food_table.food_name,food_served_table.date_created 
+						FROM food_served_table, user_table, food_table 
+						WHERE user_table.user_email like '$myusername' 
+						AND food_served_table.user_id = user_table-user_id
+						AND food_served_table.food_id = food_table.food_id");
+mysql_close();
+$events = array();
+while ($row=mysql_fetch_array($result)){            
+$id =  $row['user_table.user_id'];
+$title = $row['food_table.food_name'];
+$start = $row['food_served_table.date_created']; 
+
+$events = array(
+'id' =>  "$id",
+'name' => "$title",
+'start' => "$start"
+);
+
+}
+echo json_encode($events);
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+// List of events
+ $json = array();
+
+ // Query that retrieves events
+ $requete = "SELECT * FROM food_table ORDER BY id";
+
+ // connection to the database
+ try {
+ $bdd = new PDO('mysql:host=localhost;dbname=trackme', 'root', 'root');
+ } catch(Exception $e) {
+  exit('Unable to connect to database.');
+ }
+ // Execute the query
+ $resultat = $bdd->query($requete) or die(print_r($bdd->errorInfo()));
+
+ // sending the encoded result to success page
+ echo json_encode($resultat->fetchAll(PDO::FETCH_ASSOC));
+*/
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+try {
+	
+    // Connect to database
+    include '../mysqli_connect.php';
+	session_start();
+	$myusername = $_SESSION['myusername'];
+    // Prepare and execute query
+    $query = "SELECT * FROM food_table, user_table WHERE user_table.user_email like '$myusername' AND food_table.user_id = user_table-user_id ";
+    $sth = $connection->prepare($query);
+    $sth->execute();
+
+    // Returning array
+    $events = array();
+
+    // Fetch results
+    while ($row = $sth->fetch(PDO::FETCH_ASSOC) {
+
+        $e = array();
+        $e['id'] = $row['id'];
+        $e['title'] = "Lorem Ipsum";
+        $e['start'] = $row['start'];
+        $e['end'] = $row['end'];
+        $e['allDay'] = false;
+
+        // Merge the event array into the return array
+        array_push($events, $e);
+
+    }
+
+    // Output json for our calendar
+    echo json_encode($events);
+    exit();
+
+} catch (PDOException $e){
+    echo $e->getMessage();
+*/
+?>
--- index/profile.php
+++ index/profile.php
@@ -23,8 +23,132 @@
 		<script src='js/fullcalendar.js'></script>
 		<script type="text/javascript" src="js/bmicalc.js"></script>
 		<script>
+		
+		
 		$(document).ready(function() {
-			// page is now ready, initialize the calendar...
+			var calendar = $('#calendar').fullCalendar({
+				defaultView: 'month',
+				events: {
+					url: 'http://localhost/TrackMe/events.php',
+					type: 'POST', // Send post data
+					error: function() {
+						alert('There was an error while fetching events.');
+					}
+				}
+			});
+			
+			var date = $('#calendar').fullCalendar('today');
+			
+			$('#calendar').fullCalendar('gotoDate', date);
+		});//end of fullCalendar
+		
+		 
+		 /*
+		 $(document).ready(function() {
+			  var date = new Date();
+			  var d = date.getDate();
+			  var m = date.getMonth();
+			  var y = date.getFullYear();
+
+			  var calendar = $('#calendar').fullCalendar({
+			   editable: true,
+			   header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'month,agendaWeek,agendaDay'
+			   },
+			   
+			   events: "http://localhost/TrackMe/events.php",
+			   
+			   // Convert the allDay from string to boolean
+			   eventRender: function(event, element, view) {
+				if (event.allDay === 'true') {
+				 event.allDay = true;
+				} else {
+				 event.allDay = false;
+				}
+			   },
+			   selectable: true,
+			   selectHelper: true,
+			   select: function(start, end, allDay) {
+			   var title = prompt('Event Title:');
+			   var url = prompt('Type Event url, if exits:');
+			   if (title) {
+			   var start = $.fullCalendar.formatDate(start, "yyyy-MM-dd HH:mm:ss");
+			   var end = $.fullCalendar.formatDate(end, "yyyy-MM-dd HH:mm:ss");
+			   $.ajax({
+			   url: 'http://localhost:8888/fullcalendar/add_events.php',
+			   data: 'title='+ title+'&start='+ start +'&end='+ end +'&url='+ url ,
+			   type: "POST",
+			   success: function(json) {
+			   alert('Added Successfully');
+			   }
+			   });
+			   calendar.fullCalendar('renderEvent',
+			   {
+			   title: title,
+			   start: start,
+			   end: end,
+			   allDay: allDay
+			   },
+			   true // make the event "stick"
+			   );
+			   }
+			   calendar.fullCalendar('unselect');
+			   },
+			   
+			   editable: true,
+			   eventDrop: function(event, delta) {
+			   var start = $.fullCalendar.formatDate(event.start, "yyyy-MM-dd HH:mm:ss");
+			   var end = $.fullCalendar.formatDate(event.end, "yyyy-MM-dd HH:mm:ss");
+			   $.ajax({
+			   url: 'http://localhost:8888/fullcalendar/update_events.php',
+			   data: 'title='+ event.title+'&start='+ start +'&end='+ end +'&id='+ event.id ,
+			   type: "POST",
+			   success: function(json) {
+				alert("Updated Successfully");
+			   }
+			   });
+			   },
+			   eventResize: function(event) {
+			   var start = $.fullCalendar.formatDate(event.start, "yyyy-MM-dd HH:mm:ss");
+			   var end = $.fullCalendar.formatDate(event.end, "yyyy-MM-dd HH:mm:ss");
+			   $.ajax({
+				url: 'http://localhost:8888/fullcalendar/update_events.php',
+				data: 'title='+ event.title+'&start='+ start +'&end='+ end +'&id='+ event.id ,
+				type: "POST",
+				success: function(json) {
+				 alert("Updated Successfully");
+				}
+			   });
+
+			}
+			   
+			  });
+			  
+		});//end of fullCalendar
+		*/
+		/////////////////////////////////////////////////////////////
+		/*
+		$(document).ready(function() {
+			var calendar = $('#calendar').fullCalendar({
+				defaultView: 'month',
+				events: {
+					url: 'http://localhost/TrackMe/events.php',
+					type: 'POST', // Send post data
+					error: function() {
+						alert('There was an error while fetching events.');
+					}
+				}
+			});
+			
+			var date = $('#calendar').fullCalendar('today');
+			
+			$('#calendar').fullCalendar('gotoDate', date);
+		});//end of fullCalendar
+		*/
+				/*
+				// page is now ready, initialize the calendar...
 			
 			$('#calendar').fullCalendar({
 				defaultDate: '2015-02-12',
@@ -92,8 +216,7 @@
 				],
 		
 			});
-			
-				/*
+				////////////////////////////////////////////////////////////////////////////
 				$('#calendar').fullCalendar({
 				defaultDate: '2015-02-12',
 				editable: true,
@@ -242,9 +365,6 @@
 							
 			*/
 			//to set the calendar to today date
-			var date = $('#calendar').fullCalendar('today');
-			
-			$('#calendar').fullCalendar('gotoDate', date);
 			
 			/*
 				$('#calendar').fullCalendar({
@@ -261,7 +381,7 @@
 			
 				});
 			*/
-		});
+		//});//end of fullcalendar
 		
 		//responsible for checking all checkboxes with class name 'TimeAct'
 		function checkAllTimeAct(ele) {
