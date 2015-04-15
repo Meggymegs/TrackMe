76237166effde32c685e18ec09f08a06650c6d26
Meggymegs <suniga.miguel@yahoo.com>
2015-04-14 17:26:31 -0700

    Click on Events possible
    The application can now display more details about a specific event.

--- index/profile.php
+++ index/profile.php
@@ -11,24 +11,12 @@
 <html>
 	<head>
 		<title>Profile</title>
-		<script src='js/lib/jquery.min.js'></script>
-		<script src="js/bootstrap.min.js"></script>
-		<script src="js/typeahead.min.js"></script>
 		<link rel="stylesheet" href='css/fullcalendar/fullcalendar.css' />
 		<link rel="stylesheet" href="css/bootstrap.min.css">
 		<link rel="stylesheet" href="css/bootstrap.theme.min.css">
 		<link rel="stylesheet" href="css/profile.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<script>
-		$(document).ready(function(){
-
-			$('input.typeahead').typeahead({
-				name: 'typeahead',
-				remote:'search.php?key=%QUERY',
-				limit : 10
-			});
-		});
-		</script>
+		<script src='js/lib/jquery.min.js'></script>
 	</head>
 	<body style="background-color:#2d3e50;">
 		<nav class="navbar navbar-inverse navbar-static-top">
@@ -115,31 +103,9 @@
 						}
 					?>
 				<br>
-				<?php
-				$result = mysqli_query($dbc, "SELECT * FROM `users_table` WHERE user_email like '$myusername'"); 
-				$row = mysqli_fetch_assoc($result);
-				$tempId = $row['user_id'];
-				$result2 = mysqli_query($dbc, "SELECT * FROM `body_measurement_table` WHERE user_id like '$tempId'"); 
-				$count=mysqli_num_rows($result2);
-
-				if($count==0){
-					echo "You have not entered any information yet. Please <b>update body measurements</b>.";
-				}
-				?>
-				<b>Weight:</b>
-				<?php
-				$result = mysqli_query($dbc, "SELECT * FROM `body_measurement_table` WHERE user_id like '$tempId' AND body_measurement_type_id = 2"); 
-				$row = mysqli_fetch_assoc($result);
-				echo $row['body_measurement_value'] . " kg";
-				?>
-				<br>
-				<b>Height:</b>
-				<?php
-				$result = mysqli_query($dbc, "SELECT * FROM `body_measurement_table` WHERE user_id like '$tempId' AND body_measurement_type_id = 1"); 
-				$row = mysqli_fetch_assoc($result);
-				echo $row['body_measurement_value'] . " m";
-				?>
-				<br><br>
+				<b>Weight:</b>56kg <br>
+				<b>Height:</b>5'4" <br>
+				<b>Target&nbspWeight:</b>54kg
 			</div>
 			<div class="col-md-3"><!--form for physical act and food intake-->
 				
@@ -224,11 +190,11 @@
 							<span><p>Add food consumed<img src="res/addButton.png" style="margin-left:60px;" width="20px" height="20px"></p></span><!--<p>Click <span style="color:#AA3939;">ME</span> to add food consumed</p>-->
 							<span style="color:#AA3939;"><p>Collapse</p></span>
 						  </label>
-						  <div class="list3" style="height:100px; width: 260px; border:1px solid #E0E0E0; padding-top:5px; padding-left:5px;">
+						  <div class="list3" style="overflow-y:scroll; height:100px; width: 260px; border:1px solid #E0E0E0; padding-top:5px; padding-left:5px;">
 							<ul style="margin-left:-40px;">
-								<strong>Food</strong><span style="margin-left:40px;"><strong>&nbsp&nbsp&nbspServings & </span><span style="margin-left:2px;">Servings</strong><br>
-								<input type="text" name="typeahead" class="typeahead tt-query" autocomplete="off" spellcheck="false" placeholder="Search">
-									<select name="srvng1" value="srvng1" style="margin-left:6px; width:45px; margin-bottom:3px;">
+								<strong>Food</strong><span style="margin-left:40px;"><strong>Servings & </span><span style="margin-left:2px;">Servings</strong><br>
+								<input class="food" type="checkbox" name="food" value="egg">Fried Eggs</input> 
+									<select name="egg_srvng1" value="egg_srvng1" style="margin-left:6px; width:45px; margin-bottom:3px;">
 										<option value="0">0</option>
 										<option value="1">1</option>
 										<option value="2">2</option>
@@ -236,7 +202,7 @@
 										<option value="4">4</option>
 										<option value="5">5</option>
 									</select>
-									<select name="srvng2" value="srvng2" style="margin-left:20px; width:45px; margin-bottom:3px;">
+									<select name="egg_srvng2" value="egg_srvng2" style="margin-left:20px; width:45px; margin-bottom:3px;">
 										<option value="0">0</option>
 										<option value=".5">1/2</option>
 										<option value=".33">1/3</option>
@@ -245,9 +211,66 @@
 										<option value=".17">1/6</option>
 									</select>
 									<br><!--end of choice egg-->
+								<input class="food" type="checkbox" name="food" value="beef">Beef</input> 
+									<select name="beef_srvng1" value="beef_srvng1" style="margin-left:45.5px; width:45px; margin-bottom:3px;">
+										<option value="0">0</option>
+										<option value="1">1</option>
+										<option value="2">2</option>
+										<option value="3">3</option>
+										<option value="4">4</option>
+										<option value="5">5</option>
+									</select>
+									<select name="beef_srvng2" value="beef_srvng2" style="margin-left:20px; width:45px; margin-bottom:3px;">
+										<option value="0">0</option>
+										<option value=".5">1/2</option>
+										<option value=".33">1/3</option>
+										<option value=".25">1/4</option>
+										<option value=".2">1/5</option>
+										<option value=".17">1/6</option>
+									</select>
+									<br><!--end of choice pork-->
+								<input class="food" type="checkbox" name="food" value="chicken">Chicken</input> 
+									<select name="chicken_srvng1" value="chicken_srvng1" style="margin-left:24.3px; width:45px; margin-bottom:3px;">
+										<option value="0">0</option>
+										<option value="1">1</option>
+										<option value="2">2</option>
+										<option value="3">3</option>
+										<option value="4">4</option>
+										<option value="5">5</option>
+									</select>
+									<select name="chicken_srvng2" value="chicken_srvng2" style="margin-left:20px; width:45px; margin-bottom:3px;">
+										<option value="0">0</option>
+										<option value=".5">1/2</option>
+										<option value=".33">1/3</option>
+										<option value=".25">1/4</option>
+										<option value=".2">1/5</option>
+										<option value=".17">1/6</option>
+									</select>
+									<br><!--end of choice chicken-->
+								<input class="food" type="checkbox" name="food" value="bread">Bread</input> 
+									<select name="bread_srvng1" value="bread_srvng1" style="margin-left:36.5px; width:45px; margin-bottom:3px;">
+										<option value="0">0</option>
+										<option value="1">1</option>
+										<option value="2">2</option>
+										<option value="3">3</option>
+										<option value="4">4</option>
+										<option value="5">5</option>
+									</select>
+									<select name="bread_srvng2" value="bread_srvng2" style="margin-left:20px; width:45px;">
+										<option value="0">0</option>
+										<option value=".5">1/2</option>
+										<option value=".33">1/3</option>
+										<option value=".25">1/4</option>
+										<option value=".2">1/5</option>
+										<option value=".17">1/6</option>
+									</select>
+									<br><!--end of choice chicken-->
+									
+									<!--check all-->
+									<input type="checkbox" onchange="checkAllFood(this)" name="chk[]" />All
 							</ul>
 						  </div><!--end of list--><br>
-					<input type="submit" name="submit" value="Submit" style="width:30%; margin-left:80px; margin-top:50px; margin-bottom:8px; border-radius:4px; background-color:#AA3939; color:white;"/>
+					<input id="foodActForm" type="submit" name="submit" value="Submit"/>
 				</form>
 			</div>
 			<div class="col-md-4">
@@ -301,23 +324,21 @@
 				</form>
 			</div>	
 			<div class="col-md-6"> <!--for the calendar-->
-				<h1 align="center"><b>
-				<font size="3"><?php
+				<h1 align="center"><?php
 					$result = mysqli_query($dbc, "SELECT UPPER(first_name) AS first_name FROM `users_table` WHERE user_email like '$myusername'"); 
 					while ($row = mysqli_fetch_assoc($result)) {
 						echo $row['first_name']."'S";
 					}
-					?>&nbspCALENDAR</h1>
-					</font></b>
+					?> CALENDAR</h1>
 				<div id="calendar"></div>
 			</div>
-		</div>
+		</div><!--end of div container-->
 		
 	<!--SCRIPTS-->	
 		<script type="text/javascript" src="js/bmicalc.js"></script>
 		<script src="js/bootstrap.min.js"></script>
+		
 		<script src='js/lib/jquery.min.js'></script>
-		<script src="js/typeahead.min.js"></script>
 		<script src='js/lib/moment.min.js'></script>
 		<script src='js/fullcalendar.js'></script>
 		
@@ -334,33 +355,30 @@
 				center: 'title',
 				right: 'month,agendaWeek,agendaDay'
 			  },
-			  selectable: true,
-			  selectHelper: true,
-			  select: function(start, end, allDay) {
-			  /*
-				var title = prompt('Event Title:');
-				if (title) {
-				  calendar.fullCalendar('renderEvent',
-					{
-					  title: title,
-					  start: start,
-					  end: end,
-					  allDay: allDay
-					},
-					true // make the event "stick"
-				  );
-				}
-				calendar.fullCalendar('unselect');
-			  */
-			  },
+			  
+			  eventClick: function(calEvent, jsEvent, view) {
+					if(calEvent.data.belongsto === "food"){
+						alert('Food Name: ' + calEvent.title + '\n' + "Number of servings: " + calEvent.data.serving);
+					}
+					if(calEvent.data.belongsto === "dist_type"){
+						alert('Activity Name: ' + calEvent.title + '\n' +
+							"Distance travelled: " + calEvent.data.distance + "KM"
+							+ "\nTime: " + calEvent.data.time  + "mins");
+					}
+					if(calEvent.data.belongsto === "rep_type"){
+						alert('Activity Name: ' + calEvent.title + '\n' + "Number of Reps: " + calEvent.data.reps
+						+ "\nNumber of Sets: " + calEvent.data.sets);
+					}
+			  },//end of eventClick
+			  
 			  editable: true,
-			  eventLimit: 6, // allow "more" link when too many events
+			  eventLimit: 3, // allow "more" link when too many events
 			  events: [
 			  <?php
 			  error_reporting(0);
 					
 					$events=mysqli_query($dbc,"
-					SELECT food_name, fs.date_created 
+					SELECT food_name, fs.food_serving, fs.date_created 
 					FROM food_served_table AS fs, food_table AS f, users_table AS u 
 					WHERE fs.food_id = f.food_id 
 					AND fs.user_id = u.user_id 
@@ -369,16 +387,21 @@
 				
 				while($row = mysqli_fetch_assoc($events)) {
 				$foodName= $row['food_name'];
+				$serving= $row['food_serving'];
 				list($year,$month,$day)=explode("-", $row['date_created']);
 				?>
 				{
 				  title: '<?php echo"$foodName"?>',
 				  start: new Date(<?php echo $year; ?>,<?php echo $month; ?>-1, <?php echo $day; ?>),
+				  data: {
+					serving: '<?php echo"$serving"?>',
+					belongsto: 'food'
+				  },
 				  backgroundColor: '#34495e',
 				  borderColor: '#34495e',
 				  allDay: true
 				  
-				},
+				},//end of event array
 				<?php
 				}//end of while for food inputs
 			  ?>
@@ -386,7 +409,7 @@
 			  error_reporting(0);
 					
 					$events=mysqli_query($dbc,"
-					SELECT ptype.physical_activity_dist_type, ptable.date_created 
+					SELECT ptype.physical_activity_dist_type, ptable.distance, ptable.time, ptable.date_created 
 					FROM physical_activities_dist_table AS ptable, physical_activities_dist_type_table AS ptype, users_table AS u 
 					WHERE ptype.physical_activity_dist_id = ptable.physical_activity_dist_id 
 					AND ptable.user_id = u.user_id
@@ -395,11 +418,18 @@
 				
 				while($row = mysqli_fetch_assoc($events)) {
 				$activityName= $row['physical_activity_dist_type'];
+				$distance= $row['distance'];
+				$time= $row['time']; 
 				list($year,$month,$day)=explode("-", $row['date_created']);
 				?>
 				{
 				  title: '<?php echo"$activityName"?>',
 				  start: new Date(<?php echo $year; ?>,<?php echo $month; ?>-1, <?php echo $day; ?>),
+				  data: {
+					distance: '<?php echo"$distance"?>',
+					time: '<?php echo"$time"?>',
+					belongsto: 'dist_type'
+				  },
 				  backgroundColor: '#e74c3c',
 				  borderColor: '#e74c3c',
 				  allDay: true
@@ -412,7 +442,7 @@
 			  error_reporting(0);
 					
 					$events=mysqli_query($dbc,"
-					SELECT ptype.physical_activity_rep_type, ptable.date_created 
+					SELECT ptype.physical_activity_rep_type, ptable.number_of_reps, ptable.number_of_sets, ptable.date_created 
 					FROM physical_activities_rep_table AS ptable, physical_activities_rep_type_table AS ptype, users_table AS u 
 					WHERE ptype.physical_activity_rep_id = ptable.physical_activity_rep_id 
 					AND ptable.user_id = u.user_id
@@ -421,20 +451,26 @@
 				
 				while($row = mysqli_fetch_assoc($events)) {
 				$activityName= $row['physical_activity_rep_type'];
+				$reps= $row['number_of_reps'];
+				$sets= $row['number_of_sets'];
 				list($year,$month,$day)=explode("-", $row['date_created']);
 				?>
 				{
 				  title: '<?php echo"$activityName"?>',
 				  start: new Date(<?php echo $year; ?>,<?php echo $month; ?>-1, <?php echo $day; ?>),
+				  data: {
+					reps: '<?php echo"$reps"?>',
+					sets: '<?php echo"$sets"?>',
+					belongsto: 'rep_type'
+				  },
 				  backgroundColor: '#e74c3c',
 				  borderColor: '#e74c3c',
 				  allDay: true
-				  
 				},
 				<?php
 				}//end of while for activities inputs - reps and sets type
 			  ?>
-			  ]
+			  ]//end of eventsource
 			});
 				var date = $('#calendar').fullCalendar('today');
 				
