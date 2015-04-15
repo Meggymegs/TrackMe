84c2d2336874ee43034cc62977fb31820d375730
renzdimayuga <renz574@gmail.com>
2015-04-15 19:18:27 +0800

    Minor changes to bodyvital.php + new tables for BM and VR

---
+++ index/body_measurement_table.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.7.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 15, 2015 at 01:14 PM
+-- Server version: 5.6.20
+-- PHP Version: 5.5.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `trackme`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `body_measurement_table`
+--
+
+CREATE TABLE IF NOT EXISTS `body_measurement_table` (
+  `user_id` int(11) NOT NULL,
+  `body_measurement_type_id` int(11) NOT NULL,
+  `body_measurement_value` double NOT NULL,
+  `date_created` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
